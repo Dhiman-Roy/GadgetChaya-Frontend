733669cbe1b5a014e7ef7b7ed17c2d7f77dc097f
Dhiman-Roy <dhiman.roy7075@gmail.com>
2025-11-17 03:52:38 +0600

    new desktopNav functionality implemented

--- src/navigation/DesktopNavCategory.jsx
+++ src/navigation/DesktopNavCategory.jsx
@@ -12,13 +12,13 @@ function DesktopNavCategory({
   return (
     <div className="w-full ">
       <div className="flex justify-between w-full ">
-        {categoryData.map((category) => (
-          <div className="w-[200px] h-[100px] top-3 relative">
-            <div
-              className="flex flex-col gap-2 bg-pink-700 w-full text-center py-2  "
-              onMouseEnter={() => subCatActiveHandler(category.name)}
-              onMouseLeave={subCatCloseHandler}
-            >
+        {categoryData.map((category, index) => (
+          <div
+            className="w-[200px] h-[100px] top-3 relative   "
+            onMouseEnter={() => subCatActiveHandler(category.name)}
+            onMouseLeave={subCatCloseHandler}
+          >
+            <div className="flex flex-col gap-2 bg-pink-700 w-full text-center py-2  ">
               {category.name}
             </div>
             {subCatActive === category.name &&
@@ -28,6 +28,8 @@ function DesktopNavCategory({
                   subSubCatActive={subSubCatActive}
                   subSubCatActiveHandler={subSubCatActiveHandler}
                   subSubCatCloseHandler={subSubCatCloseHandler}
+                  categoryDataLength={categoryData.length}
+                  index={index}
                 />
               )}
           </div>
--- src/navigation/DesktopNavSubCategory.jsx
+++ src/navigation/DesktopNavSubCategory.jsx
@@ -1,40 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function DesktopNavSubCategory({
   category,
   subSubCatActive,
   subSubCatActiveHandler,
   subSubCatCloseHandler,
+  categoryDataLength,
+  index,
 }) {
   return (
-    <div className="absolute top-9 z-30">
-      {category.subCategories.map((subcat) => (
-        <div
-          className="flex flex-col gap-2"
-          onMouseEnter={() => subSubCatActiveHandler(subcat.name)}
-          onMouseLeave={subSubCatCloseHandler}
-        >
-          <div className="bg-green-400">{subcat.name}//</div>
-          {subcat?.subSubCategories?.length > 0 &&
-            subSubCatActive === subcat.name &&
-            subcat.subSubCategories.map((subSubCat) => (
-              <div
-                className="flex flex-col w-[200px] gap2 bg-rose-500 "
-                onMouseEnter={subSubCatActiveHandler(subcat.name)}
-                onMouseLeave={subSubCatCloseHandler}
-              >
-                <div>{subSubCat.name}</div>
-              </div>
-            ))}
-          {/* {subSubCatActive &&
+    <div className="absolute top-9 z-30 text-center">
+      {category.subCategories.map((subcat) => {
+        return (
+          <div
+            className="flex flex-col cursor-pointer"
+            onMouseEnter={() => subSubCatActiveHandler(subcat.name)}
+            onMouseLeave={subSubCatCloseHandler}
+          >
+            <div className="bg-green-400">{subcat.name}</div>
+            <div
+              className={`cursor-pointer absolute top-0 ${
+                index > 4
+                  ? "-translate-x-full left-0"
+                  : "translate-x-full right-0"
+              } `}
+            >
+              {subcat?.subSubCategories?.length > 0 &&
+                subSubCatActive === subcat.name &&
+                subcat.subSubCategories.map((subSubCat) => (
+                  <div
+                    className="flex flex-col w-[200px] bg-rose-500 p-2 "
+                    onMouseEnter={subSubCatActiveHandler(subcat.name)}
+                    onMouseLeave={subSubCatCloseHandler}
+                  >
+                    {subSubCat.name}
+                  </div>
+                ))}
+            </div>
+
+            {/* {subSubCatActive &&
             subcat.subSubCategories &&
             subcat.subSubCategories.map((subSubCat) => (
               <div className="flex flex-col ">
                 <div>{subSubCat.name}</div>
               </div>
             ))} */}
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
